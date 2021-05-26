5c7f10ffbec6cadbb93a456853eb27d2995e43a9
Alberto <alby_b_@hotmail.it>
2021-05-26 17:44:51 +0200

    network added and build.gradle

---
+++ app/src/main/java/com/benni/getyourrecipe/network/model/RecipeNetworkEntity.kt
@@ -0,0 +1,37 @@
+package com.benni.getyourrecipe.network.model
+
+import com.google.gson.annotations.SerializedName
+
+class RecipeNetworkEntity (
+
+    @SerializedName(value = "pk")
+    var pk:Int? = null,
+
+    @SerializedName(value = "title")
+    var title: String? = null,
+
+    @SerializedName(value = "publisher")
+    var publisher:String? = null,
+
+    @SerializedName(value = "rating")
+    var rating:Int? = null,
+
+    @SerializedName(value = "source_url")
+    var source_url: String? = null,
+
+    @SerializedName(value = "description")
+    var description: String? = null,
+
+    @SerializedName(value = "cooking_instructions")
+    var cookingInstructions: String? = null,
+
+    @SerializedName(value = "ingredients")
+    var ingredients: List<String>? = null,
+
+    @SerializedName(value = "date_added")
+    var dateAdded: String? = null,
+
+    @SerializedName(value = "date_updated")
+    var dateUpdated: String? = null,
+
+    )
