cc6239091bb692874737fa52e711f4672e221e6d
Alberto <alby_b_@hotmail.it>
2021-06-04 12:46:11 +0200

    main activity commented

--- app/src/main/java/com/benni/getyourrecipe/MainActivity.kt
+++ app/src/main/java/com/benni/getyourrecipe/MainActivity.kt
@@ -19,19 +19,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+// TODO Test get request - 3/6 working
 
-        val service = Retrofit.Builder()
-            .baseUrl("https://food2fork.ca/api/recipe/")
-            .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
-            .build()
-            .create(RecipeService::class.java)
+//        val service = Retrofit.Builder()
+//            .baseUrl("https://food2fork.ca/api/recipe/")
+//            .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
+//            .build()
+//            .create(RecipeService::class.java)
+//
+//        CoroutineScope(IO).launch {
+//            val recipe = service.get(
+//                token = "Token 9c8b06d329136da358c2d00e76946b0111ce2c48",
+//                id = 584
+//            )
+//            Log.d("MAIN ACT", "onCreate: ${recipe.title}")
+//        }
 
-        CoroutineScope(IO).launch {
-            val recipe = service.get(
-                token = "Token 9c8b06d329136da358c2d00e76946b0111ce2c48",
-                id = 584
-            )
-            Log.d("MAIN ACT", "onCreate: ${recipe.title}")
-        }
+
     }
 }
